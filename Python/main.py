f688a1689d350298c49422ed99a1bb35bf77b02d
Zonas <zonastharu@gmail.com>
2023-12-29 22:23:20 +0545

    First Commit

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+mylist = ["apple","banana","Orange","Mango","Kiwi"]
+mylist[2] = "Pineapple"
+print(mylist)
+mylist[1] = "Banana"
+print(mylist)
+mylist.insert(0,"Dragon Fruit")
+print(mylist)
+mylist.append("Carrot")
+print(mylist)
+myanother = ["Meat","Fish","Chicken"]
+mylist.extend(myanother)
+print(mylist)
+mylist.remove("Pineapple")
+print(mylist)
+mylist.pop(3)
+print(mylist)
