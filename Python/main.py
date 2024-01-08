bf8c670bfd9b56af65e1a4f7840ada2c57a22bf0
Zonas <zonastharu@gmail.com>
2024-01-08 23:53:28 +0545

    Learned upto Dictoniries Access Item

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,10 @@
-myset = {"orange","apple","Mango"}
-myset2 = {"pineapple","blueberry","apple"}
-x = myset.intersection(myset2)
-print(x)
+mydoc = {
+    "Name" : "John",
+    "Surname" : "Tharu",
+    "Age" : "24",
+    "School" : "Shree Pashupati Higher Secondary School",
+    "College" : "Crimson College of Technology" 
+}
+mydoc["Age"] = 25
+mydoc.update({"Age" : 26})
+print(mydoc.items())
