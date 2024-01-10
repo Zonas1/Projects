510a31e77eb4d3e006cfbd15b043289057956b97
Zonas <zonastharu@gmail.com>
2024-01-10 23:21:58 +0545

    Learned Upto Function

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,5 @@
-mydoc = {
-    "Name" : "John",
-    "Surname" : "Tharu",
-    "Age" : "24",
-    "School" : "Shree Pashupati Higher Secondary School",
-    "College" : "Crimson College of Technology" 
-}
-mydoc["Age"] = 25
-mydoc.update({"Age" : 26})
-print(mydoc.items())
+def myfun(name,add,country):
+    print("Your Name is : " + name + " and you live in "+add+" place of "+country)
+myfun("John","Kotihawa","Nepal")
+myfun("Joey","Madhawaliya","Nepal")
+myfun(name = "Hiro",add = "Tilottama",country = "Nepal")
