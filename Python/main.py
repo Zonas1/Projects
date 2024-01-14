57eecc24844c19ee74c8b79b9b9d00099cba8781
Zonas <zonastharu@gmail.com>
2024-01-14 23:14:00 +0545

    Learned upto Maths

--- Python/main.py
+++ Python/main.py
@@ -1,36 +1,4 @@
-class John():
-    def __init__(self,name,add):
-        self.name = name
-        self.add = add
-p1 = John("John Kathariya Tharu","Tilottama-15, Kotihawa")
-p1.name = "John Tharu"
-print(p1.name)
-p1.add = "Kotihawa"
-print(p1.add)
+import datetime
 
-class parson():
-    def __init__(self,fname,lname):
-        self.fname= fname
-        self.lname = lname
-    def prinname(self):
-        print(self.fname+ " and "+self.lname)
-p1 = parson("John Kathariya","Tharu")
-p1.prinname()
-
-class Student(parson):
-    pass
-
-p2 = Student("Hero Prasad","Tharu")
-p2.prinname()
-
-
-class Joey():
-    def __init__(self,fname,lname):
-        self.fname = fname
-        self.lname = lname
-class Bishal(Joey):
-    def __init__(self, fname, lname):
-        super().__init__(fname, lname)
-        print(fname+" and "+lname)
-
-p1 = Bishal("Joey","Tharu")
+x = datetime.datetime(2024,5,28)
+print(x)
---
+++ Python/mymodule.py
@@ -0,0 +1,9 @@
+def myfun():
+    def move(self):
+        print("I am moving")
+
+person = {
+    "Name" : "John",
+    "Surname" : "Tharu",
+    "Age" : 24
+}
