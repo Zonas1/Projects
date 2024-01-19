8db4a157d1df70304687017e52603111099a4416
Zonas <zonastharu@gmail.com>
2024-01-19 23:50:04 +0545

    Learned upto Matplotlib Line

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as mp
 import numpy as np
-x = np.array([[1,2,3],[4,5,6]])
-print(x.shape)
+x1 = np.array([1,2,3,7])
+y1 = np.array([2,4,5,2])
+x2 = np.array([1,3,5,7])
+y2 = np.array([6,4,6,8])
+mp.plot(x1,y1,x2,y2)
+mp.show()
